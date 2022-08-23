2f11eb946b871903e6634a5e47765eb15074987c
Dominik Bullo <dominik.bullo@gmail.com>
2022-08-23 23:38:40 +0200

    refactor(db, settings): Adjusted migrations and minor changes in project setup.

--- migrations/20220818170127-galleries-table.js
+++ migrations/20220818170127-galleries-table.js
@@ -19,7 +19,7 @@ exports.up = function (db) {
         id: { type: 'int', primaryKey: true, autoIncrement: true },
         title: { type: 'string', length: 255, notNull: true },
         path: { type: 'string', length: 255, notNull: true },
-        date: { type: 'date', notNull: true },
+        date: { type: 'string', notNull: false },
     })
 }
 
